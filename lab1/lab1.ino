579871eec0088404d97510628de5f408db358ae7
rkhapov <r.khapov@yandex.ru>
2019-10-26 10:55:17 +0500

    task

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,18 +1,39 @@
-#include "pitches.h"
+#include <Arduino.h>
+#include <MD_TCS230.h>
+
 #include "button.h"
-#include "buzzer.h"
+
+#define  S0_OUT  2
+#define  S1_OUT  3
+#define  S2_OUT  4
+#define  S3_OUT  5
 
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
 
+#define R_CONTROL_PIN 9
+#define G_CONTROL_PIN 10
+#define B_CONTROL_PIN 11
+
 MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
+
+bool rEnabled = true;
+bool gEnabled = true;
+bool bEnabled = true;
+
+Button rButton(R_CONTROL_PIN);
+Button gButton(G_CONTROL_PIN);
+Button bButton(B_CONTROL_PIN);
 
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Started!");
 
+    sensorData whiteCalibration;
+    whiteCalibration.value[TCS230_RGB_R] = 0;
+    whiteCalibration.value[TCS230_RGB_G] = 0;
     whiteCalibration.value[TCS230_RGB_B] = 0;
 
     sensorData blackCalibration;
@@ -33,27 +54,46 @@ void loop()
 {
     colorData rgb;
     colorSensor.read();
-
     while (!colorSensor.available());
 
-    colorSensor.getRGB(&rgb);
-    print_rgb(rgb);
+    if (rButton.wasPressed()){
+      rEnabled = !rEnabled;
+    }
+
+    if (gButton.wasPressed()) {
+      gEnabled = !gEnabled;
+    }
+
+    if (bButton.wasPressed()){
+      bEnabled = !bEnabled;
+    }
+    
+    colorSensor.getRGB(&rgb); 
     set_rgb_led(rgb);
 }
 
-void print_rgb(colorData rgb)
-{
-  Serial.print(rgb.value[TCS230_RGB_R]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_G]);
-  Serial.print(" ");
-  Serial.print(rgb.value[TCS230_RGB_B]);
-  Serial.println();
-}
-  
 void set_rgb_led(colorData rgb)
 {
-    analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
-    analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
-    analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+    if (rEnabled) {
+      analogWrite(R_OUT, 255 - rgb.value[TCS230_RGB_R]);
+    }
+    else {
+      analogWrite(R_OUT, 255);
+    }
+
+
+    if (gEnabled) {
+      analogWrite(G_OUT, 255 - rgb.value[TCS230_RGB_G]);
+    }
+    else {
+      analogWrite(G_OUT, 255);
+    }
+
+
+    if (bEnabled) {
+      analogWrite(B_OUT, 255 - rgb.value[TCS230_RGB_B]);
+    }
+    else {
+      analogWrite(B_OUT, 255);
+    }
 }
